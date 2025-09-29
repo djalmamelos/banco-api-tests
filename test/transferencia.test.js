5e0d525888295c604c6995f01d36cbd3f4ed962b
Djalma Melo S. <djalmamelos@gmail.com>
2025-09-29 09:56:50 -0300

    adicionando fixtures

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 const { expect } = require('chai')
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencias.json')
+
 
 describe('Transferencias', () => {
     describe('POST/transferencias', () => {
@@ -12,33 +14,30 @@ describe('Transferencias', () => {
         })
 
         it('Deve Retornar sucesso 201 quando o valor da transferencia igual ou acima de R$ 10,00 ', async () => {
+            const bodyTransferencias = { ...postTransferencias }
 
-        const resposta = await request(process.env.BASE_URL)
-            .post('/transferencias')
-            .set('Content-Type', 'application/json')
-            .set('Authorization', `Bearer ${token}`)
-            .send({
-                contaOrigem: 1,
-                contaDestino: 2,
-                valor: 11,
-                token: ""
-            })
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send(
+                    bodyTransferencias
+                )
 
-        expect(resposta.status).to.equal(201);
-    });
-    
-    it('Deve Retornar  falha 422 quando o valor da transferencia abaixo de R$ 10,00 ', async () => {
+            expect(resposta.status).to.equal(201);
+        });
 
-        const resposta = await request(process.env.BASE_URL)
-            .post('/transferencias')
-            .set('Content-Type', 'application/json')
-            .set('Authorization', `Bearer ${token}`)
-            .send({
-                contaOrigem: 1,
-                contaDestino: 2,
-                valor: 2,
-                token: ""
-            })
-    });
-})
+        it('Deve Retornar  falha 422 quando o valor da transferencia abaixo de R$ 10,00 ', async () => {
+            const bodyTransferencias = { ...postTransferencias }
+            bodyTransferencias.valor = 2
+            const resposta = await request(process.env.BASE_URL)
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send(
+                   bodyTransferencias
+                )
+                expect(resposta.status).to.equal(422);
+        });
+    })
 })
