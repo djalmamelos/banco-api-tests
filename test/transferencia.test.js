e657aafaa0d0b82f92b9ba2f85ebfedab5efee2c
Djalma Melo S. <djalmamelos@gmail.com>
2025-09-29 08:48:42 -0300

    adicioanndo hook de BeforeEach

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,36 +5,40 @@ const { obterToken } = require('../helpers/autenticacao')
 
 describe('Transferencias', () => {
     describe('POST/transferencias', () => {
+        let token
+
+        beforeEach(async () => {
+            token = await obterToken('julio.lima', '123456')
+        })
+
         it('Deve Retornar sucesso 201 quando o valor da transferencia igual ou acima de R$ 10,00 ', async () => {
-            // capturar o token 
-            const token = await obterToken
 
-            const resposta = await request(process.env.BASE_URL)
-                .post('/transferencias')
-                .set('Content-Type', 'application/json')
-                .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 11,
-                    token: ""
-                })
+        const resposta = await request(process.env.BASE_URL)
+            .post('/transferencias')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                contaOrigem: 1,
+                contaDestino: 2,
+                valor: 11,
+                token: ""
+            })
 
-            expect(resposta.status).to.equal(201);
-        });
-        it('Deve Retornar  falha 422 quando o valor da transferencia abaixo de R$ 10,00 ', async () => {
-            const token = await obterToken
+        expect(resposta.status).to.equal(201);
+    });
+    
+    it('Deve Retornar  falha 422 quando o valor da transferencia abaixo de R$ 10,00 ', async () => {
 
-            const resposta = await request(process.env.BASE_URL)
-                .post('/transferencias')
-                .set('Content-Type', 'application/json')
-                .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 2,
-                    token: ""
-                })
-        });
-    })
+        const resposta = await request(process.env.BASE_URL)
+            .post('/transferencias')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                contaOrigem: 1,
+                contaDestino: 2,
+                valor: 2,
+                token: ""
+            })
+    });
+})
 })
